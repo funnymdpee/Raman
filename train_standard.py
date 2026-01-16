a4e5d663670477beaa47b7fd7896f65e1f953fca
wzc <2804303353@qq.com>
2026-01-16 11:46:42 +0800

    同上

--- train_standard.py
+++ train_standard.py
@@ -32,7 +32,7 @@ def train_one_epoch(model, loader, optimizer, device):
     model.train()
     total_loss = 0.0
     # criterion = nn.MSELoss()
-    criterion = HybridLoss(grad_weight=0).to(device)
+    criterion = HybridLoss(grad_weight=0.1).to(device)
 
     for y_batch, x_batch in tqdm(loader, desc="Train", leave=False):
         y, x = y_batch.to(device), x_batch.to(device)
